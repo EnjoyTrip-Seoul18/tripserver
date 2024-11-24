22e55006b2894c69355e9b483a28bfb272d4b70e
ziy00n <jiyun4188@gmail.com>
2024-11-24 15:16:10 +0900

    [feat] 로그인 사용자 인증 제외하는 경로 추가

--- src/main/java/com/ssafy/trip/config/WebConfiguration.java
+++ src/main/java/com/ssafy/trip/config/WebConfiguration.java
@@ -47,7 +47,9 @@ public class WebConfiguration implements WebMvcConfigurer {
 						"/swagger-resources/**",
 						"/test1",
 						"/member/login",
-						"/member/join"
+						"/member/join",
+						"/attraction/**",
+						"/board/list"
 				);
 		//주의: 인터셉터에서 동일한 엔드포인트 url인데 HTTP METHOD가 다른경우 구분 불가능
 	}
