522f2894f0b271c59aa7618484652f9a482750f1
ziy00n <jiyun4188@gmail.com>
2024-11-24 18:00:01 +0900

    ✨ [feat] 게시글 리스트 조회(검색 기능 포함), 게시글 상세조회

---
+++ src/main/java/com/ssafy/trip/dto/board/BoardListResponse.java
@@ -0,0 +1,26 @@
+package com.ssafy.trip.dto.board;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Schema(title = "BoardListDto : 게시글 목록 & 페이지 정보", description = "게시글 목록과 현재 페이지와 전체 페이지 정보를 나타낸다.")
+public class BoardListResponse {
+
+    @Schema(description = "글목록")
+    private List<BoardDto> articles;
+    @Schema(description = "현재 페이지번호")
+    private int currentPage;
+    @Schema(description = "전체 페이지 수")
+    private int totalPageCount;
+}
+
